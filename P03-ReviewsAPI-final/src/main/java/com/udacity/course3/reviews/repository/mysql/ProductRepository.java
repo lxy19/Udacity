abcda6aa97986b38b00b2782cea1854bbe994092
Xiangye Li <xiangyeli@gmail.com>
2019-11-03 20:22:11 -0500

    remove

--- P03-ReviewsAPI-final/src/main/java/com/udacity/course3/reviews/repository/mysql/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.udacity.course3.reviews.repository.mysql;
-
-import com.udacity.course3.reviews.entity.mysql.Product;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProductRepository extends CrudRepository<Product, Integer> {
-}
